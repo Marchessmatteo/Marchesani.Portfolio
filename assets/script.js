4b4be16b6769681d26cbfcaff1edc387d6f02fab
Marchessmatteo <matteo98.marchesani@gmail.com>
2023-02-18 19:34:17 +0100

    script
    emailjs

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,32 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+function sendEmail() {
+    // Ottenere i dati dal form
+    let form = document.getElementById("my-form");
+    let name = form.elements["Nome"].value;
+    let email = form.elements["Email"].value;
+    let subject = form.elements["Argomento"].value;
+    let message = form.elements["Dettaglio del progetto"].value;
+
+    emailjs.send("service_02y6c1o", "template_181yc5c", {
+        from_name: Nome,
+        from_email: Email,
+        subject: Argomento,
+        message: Dettaglio del progetto,
+      }).then(
+        function(response) {
+          console.log("SUCCESS", response);
+          alert("Messaggio inviato correttamente!");
+        },
+        function(error) {
+          console.log("FAILED", error);
+          alert("Errore nell'invio del messaggio. Riprova più tardi.");
+        }
+      );
+    
+      // Resetta il form
+      form.reset();
+      return false;
+    }
+  
