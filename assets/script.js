c543d1dd155c2859134b0aae8007eb330c501cad
Marchessmatteo <matteo98.marchesani@gmail.com>
2023-02-20 22:36:17 +0100

    1
    s

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,28 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
-var feedbackElement = document.getElementById("feedback");
-
-emailjs.send('service_tnba52n', 'template_9dcyoxu', templateParams)
-  .then(function(response) {
-    feedbackElement.innerHTML = "Messaggio inviato con successo!";
-  })
-  .catch(function(error) {
-    feedbackElement.innerHTML = "Si è verificato un errore nell'invio del messaggio: " + error;
+document.getElementById('submit-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    // prendi i dati della form
+    var templateParams = {
+      Nome: document.getElementById('myForm').elements[0].value,
+      Email: document.getElementById('myForm').elements[1].value,
+      Argomento: document.getElementById('myForm').elements[2].value,
+      Dettagli: document.getElementById('myForm').elements[3].value
+    };
+  
+    // invia l'email utilizzando EmailJS
+    emailjs.send('service_tnba52n', 'template_9dcyoxu', templateParams)
+      .then(function(response) {
+        // se l'invio è riuscito, mostra un messaggio di successo
+        var feedbackElement = document.getElementById('feedback');
+        feedbackElement.innerHTML = 'Messaggio inviato con successo!';
+        feedbackElement.classList.add('success');
+      }, function(error) {
+        // se l'invio è fallito, mostra un messaggio di errore
+        var feedbackElement = document.getElementById('feedback');
+        feedbackElement.innerHTML = 'Si è verificato un errore nell\'invio del messaggio: ' + error;
+        feedbackElement.classList.add('error');
+      });
   });
-
+  
